b65bc33d1b4a5eaffe3d50a1450563c2a98eba9f
michaeldobosh <muxa_dobosh@mail.ru>
2023-11-12 19:04:02 +0800

    second step

--- src/index.js
+++ src/index.js
@@ -5,36 +5,35 @@ import axios from 'axios';
 import renameFromUrl from '../utils/renameFromUrl.js';
 import parser from './parser.js';
 
-const writeFiles = (files, filesUrls, directoryPath, domenUrl) => Promise.all(files)
-  .then((responses) => {
-    responses.forEach((file, i) => {
-      const { ext } = path.parse(filesUrls[i]);
-      if (file?.data) {
-        fsp.writeFile(path.join(directoryPath, `${renameFromUrl(filesUrls[i], domenUrl)}${ext}`), file?.data);
-      }
-    });
-  })
-  .catch((err) => console.log(err.message));
-
 export default (link, output) => {
   const url = new URL(link);
   const renamedUrl = renameFromUrl(url.toString(), url.origin);
   const fileName = `${renamedUrl}.html`;
   const directoryFileName = `${renamedUrl}_files`;
   const filepath = path.resolve(output, fileName);
-  const directoryPath = path.join(output, directoryFileName);
+  const pathToFileDirectory = path.join(output, directoryFileName);
+  let urls;
 
   return axios.get(url.toString())
     .then(({ data }) => {
       const { htmlData, filesUrls } = parser(data, url.origin, directoryFileName);
-      fsp.mkdir(directoryPath);
-      fsp.writeFile(filepath, htmlData, 'utf-8');
-      const files = filesUrls.map((fileUrl) => axios({ method: 'get', url: fileUrl, responseType: 'stream' }));
-      return { htmlData, files, filesUrls };
-    })
-    .then(({ htmlData, files, filesUrls }) => {
-      writeFiles(files, filesUrls, directoryPath, url.origin);
+      urls = filesUrls;
+      fsp.mkdir(pathToFileDirectory);
       return htmlData;
+    })
+    .then((htmlData) => {
+      fsp.writeFile(filepath, htmlData, 'utf-8');
+      const requests = urls.map((fileUrl) => axios({ method: 'get', url: fileUrl, responseType: 'stream' }));
+      return requests;
+    })
+    .then((requests) => Promise.all(requests))
+    .then((responses) => {
+      responses.forEach((respons, i) => {
+        const { ext } = path.parse(urls[i]);
+        if (respons?.data) {
+          fsp.writeFile(path.join(pathToFileDirectory, `${renameFromUrl(urls[i], url.origin)}${ext}`), respons.data);
+        }
+      });
     })
     .then(() => filepath)
     .catch((err) => console.log(err));
