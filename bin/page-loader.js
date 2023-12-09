463017fd6d6adf603359de23a19a94010e9723d3
michaeldobosh <muxa_dobosh@mail.ru>
2023-12-09 09:43:52 +0800

    upd

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -13,26 +13,26 @@ program
     `output dir (default: "${process.cwd()}")`,
     process.cwd(),
   )
-  .action((url, dir) => pageloader(url, dir.output))
-  .then((pathToFile) => console.log(`Page was successfully downloaded into ${pathToFile}`))
-  .catch((error) => {
-    if (error.errno === -2) {
-      console.log(`Cannot write ${error.path}: no such file or directory '${path.parse(error.path).dir}'`);
+  .action((url, dir) => pageloader(url, dir.output)
+    .then((pathToFile) => console.log(`Page was successfully downloaded into ${pathToFile}`))
+    .catch((error) => {
+      if (error.errno === -2) {
+        console.error(`Cannot write ${error.path}: no such file or directory '${path.parse(error.path).dir}'`);
+        process.exit(1);
+      }
+      if (error.errno === -13) {
+        console.error(`Cannot write ${error.path}: Permission denied`);
+        process.exit(1);
+      }
+      if (error.errno === -17) {
+        console.error(`Cannot write ${error.path}: already exists`);
+        process.exit(1);
+      }
+      if (error.errno === -3001 || error.errno === -3008) {
+        console.error('No response');
+        process.exit(1);
+      }
       process.exit(1);
-    }
-    if (error.errno === -13) {
-      console.log(`Cannot write ${error.path}: Permission denied`);
-      process.exit(1);
-    }
-    if (error.errno === -17) {
-      console.log(`Cannot write ${error.path}: already exists`);
-      process.exit(1);
-    }
-    if (error.errno === -3001 || error.errno === -3008) {
-      console.log('No response');
-      process.exit(1);
-    }
-    process.exit(1);
-  });
+    }));
 
 program.parse();
